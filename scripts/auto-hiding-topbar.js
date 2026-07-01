bb4f40424fbc87a4b0ef01c1bddbd217d17a2072
ketherball <121590283+ketherball@users.noreply.github.com>
2026-07-01 18:12:28 -0400

    Refactor autohiding topbar script to only update class list if necessary

--- scripts/auto-hiding-topbar.js
+++ scripts/auto-hiding-topbar.js
@@ -4,15 +4,18 @@
     const topbar = document.querySelector('.topbar');
     let lastScrollY = window.scrollY;
 
+    let isHidden = true;
+
     window.addEventListener('scroll', (e) => {
         const currentScrollY = window.scrollY;
 
-        if (currentScrollY < lastScrollY) {
+        if (isHidden === false && currentScrollY < lastScrollY) {
             topbar.classList.remove('hide');
+            isHidden = true;
 
-        } else if (currentScrollY > lastScrollY) {
+        } else if (isHidden === true && currentScrollY > lastScrollY) {
             topbar.classList.add('hide');
-
+            isHidden = false;
         }
 
         lastScrollY = currentScrollY;
