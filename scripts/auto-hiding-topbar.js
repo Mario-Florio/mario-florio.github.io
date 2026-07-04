2af3b52eaaab8a54e1c4ab2b02527302354ae01b
ketherball <121590283+ketherball@users.noreply.github.com>
2026-07-04 09:07:26 -0400

    Fix variable naming

--- scripts/auto-hiding-topbar.js
+++ scripts/auto-hiding-topbar.js
@@ -4,18 +4,18 @@
     const topbar = document.querySelector('.topbar');
     let lastScrollY = window.scrollY;
 
-    let isHidden = true;
+    let isHidden = false;
 
     window.addEventListener('scroll', (e) => {
         const currentScrollY = window.scrollY;
 
-        if (isHidden === false && currentScrollY < lastScrollY) {
+        if (isHidden === true && currentScrollY < lastScrollY) {
             topbar.classList.remove('hide');
-            isHidden = true;
-
-        } else if (isHidden === true && currentScrollY > lastScrollY) {
-            topbar.classList.add('hide');
             isHidden = false;
+
+        } else if (isHidden === false && currentScrollY > lastScrollY) {
+            topbar.classList.add('hide');
+            isHidden = true;
         }
 
         lastScrollY = currentScrollY;
